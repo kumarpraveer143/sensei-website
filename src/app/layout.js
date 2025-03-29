3595df6609648641e7f006e64297b630ffc97eba
Suyash Srivastava <svtsuyash@gmail.com>
2025-03-29 14:20:45 +0530

    analytics tag added

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,11 +2,13 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import logo from "@/assets/Logo.svg?url";
-import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
+// import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 // import { ToastContainer } from "react-toastify";
 import StoreProvider from "@/Redux/Provider";
+import { GoogleTagManager, GoogleTagManagerNoScript, MetaPixel, GTag } from "@/components/Analytics";
+
 export const metadata = {
   // metadataBase: new URL(`${process.env.NEXTAUTH_URL}`),
   title: {
@@ -25,13 +27,17 @@ export default function RootLayout({ children }) {
   return (
     <>
       <html lang="en scrollbar-hide">
+        {/* <GoogleTagManager gtmId={process.env.GTM_ID} /> */}
+        <GTag gtagId={process.env.GTAG_ID} />
         <GoogleTagManager gtmId={process.env.GTM_ID} />
+        <MetaPixel pixelId={process.env.META_PIXEL_ID} />
         <body
           className={
             "scrollbar-hide flex min-h-screen flex-col justify-between"
           }
         >
-          <GoogleAnalytics gaId={process.env.GA_ID} />
+          <GoogleTagManagerNoScript gtmId={process.env.GTM_ID} />
+          {/* <GoogleAnalytics gaId={process.env.GA_ID} /> */}
           <StoreProvider>
             <Navbar />
             {children}
